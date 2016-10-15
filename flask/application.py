317afbbef5d3463780bb803ec515307222731b96
Maddie Cousens <mscousens@gmail.com>
2016-10-15 22:19:47 +0000

    adding original files from flask assessment

---
+++ flask/application.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+
+app = Flask(__name__)
+
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+@app.route("/")
+def index_page():
+    """Show an index page."""
+
+    return "<html><body>This is the homepage.</body></html>"
+
+    # Alternately, we could make this a Jinja template in `templates/`
+    # and return that result of rendering this, like:
+    #
+    # return render_template("index.html")
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
+
