8dd318223e880bb6fa207f4d5055ae268abff3ba
Maddie Cousens <mscousens@gmail.com>
2016-11-20 20:09:00 +0000

    Finished tree

--- cs-data-struct-1/tree.py
+++ cs-data-struct-1/tree.py
@@ -26,9 +26,7 @@ class Node(object):
             2
         """
 
-        # FIXME
-
-        pass
+        return len(self.children)
 
 
 class Tree(object):
@@ -93,8 +91,15 @@ class Tree(object):
 
         """
 
-        # FIXME
-        pass
+        to_visit = [self.root]
+
+        while to_visit:
+            node = to_visit.pop(0)
+
+            if node.data == data:
+                return node
+
+            to_visit.extend(node.children)
 
 if __name__ == "__main__":
     import doctest
